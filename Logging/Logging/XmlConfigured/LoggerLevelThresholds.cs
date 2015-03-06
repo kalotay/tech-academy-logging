db61ac3f4471d53db7cd66dcf21947b374353c6d
Daniel Kalotay <daniel.kalotay@7digital.com>
2015-03-06 14:18:14 +0000

    logger levels

---
+++ Logging/Logging/XmlConfigured/LoggerLevelThresholds.cs
@@ -0,0 +1,64 @@
+﻿using NUnit.Framework;
+using log4net;
+
+namespace Logging.XmlConfigured
+{
+	[TestFixture]
+	public class LoggerLevelThresholds
+	{
+		[Test]
+		public void FatalLoggerOnlyHasFatalEnabled()
+		{
+			var log = LogManager.GetLogger("FatalLogger");
+			Assert.That(log.IsFatalEnabled, "Fatal should be enabled");
+			Assert.That(log.IsErrorEnabled, Is.False, "Error should be disabled");
+			Assert.That(log.IsWarnEnabled, Is.False, "Warn should be disabled");
+			Assert.That(log.IsInfoEnabled, Is.False, "Info should be disabled");
+			Assert.That(log.IsDebugEnabled, Is.False, "Debug should be disabled");
+		}
+
+		[Test]
+		public void ErrorLoggerOnlyHasErrorEnabled()
+		{
+			var log = LogManager.GetLogger("ErrorLogger");
+			Assert.That(log.IsFatalEnabled, "Fatal should be enabled");
+			Assert.That(log.IsErrorEnabled, "Error should be enabled");
+			Assert.That(log.IsWarnEnabled, Is.False, "Warn should be disabled");
+			Assert.That(log.IsInfoEnabled, Is.False, "Info should be disabled");
+			Assert.That(log.IsDebugEnabled, Is.False, "Debug should be disabled");
+		}
+
+		[Test]
+		public void WarnLoggerOnlyHasWarnEnabled()
+		{
+			var log = LogManager.GetLogger("WarnLogger");
+			Assert.That(log.IsFatalEnabled, "Fatal should be enabled");
+			Assert.That(log.IsErrorEnabled, "Error should be enabled");
+			Assert.That(log.IsWarnEnabled, "Warn should be enabled");
+			Assert.That(log.IsInfoEnabled, Is.False, "Info should be disabled");
+			Assert.That(log.IsDebugEnabled, Is.False, "Debug should be disabled");
+		}
+
+		[Test]
+		public void InfoLoggerOnlyHasInfoEnabled()
+		{
+			var log = LogManager.GetLogger("InfoLogger");
+			Assert.That(log.IsFatalEnabled, "Fatal should be enabled");
+			Assert.That(log.IsErrorEnabled, "Error should be enabled");
+			Assert.That(log.IsWarnEnabled, "Warn should be enabled");
+			Assert.That(log.IsInfoEnabled, "Info should be enabled");
+			Assert.That(log.IsDebugEnabled, Is.False, "Debug should be disabled");
+		}
+
+		[Test]
+		public void DebugLoggerOnlyHasDebugEnabled()
+		{
+			var log = LogManager.GetLogger("DebugLogger");
+			Assert.That(log.IsFatalEnabled, "Fatal should be enabled");
+			Assert.That(log.IsErrorEnabled, "Error should be enabled");
+			Assert.That(log.IsWarnEnabled, "Warn should be enabled");
+			Assert.That(log.IsInfoEnabled, "Info should be enabled");
+			Assert.That(log.IsDebugEnabled, "Debug should be enabled");
+		}
+	}
+}
