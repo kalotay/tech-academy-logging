b863c39a79cb05f41de6ed8380b6b5f15c36d93c
Daniel Kalotay <daniel.kalotay@7digital.com>
2015-03-09 13:26:05 +0000

    custom appender

---
+++ Logging/Logging/NoopAppender.cs
@@ -0,0 +1,13 @@
+﻿using log4net.Appender;
+using log4net.Core;
+
+namespace Logging
+{
+	public class NoopAppender: AppenderSkeleton
+	{
+		protected override void Append(LoggingEvent loggingEvent)
+		{}
+
+		public object Token { get; set; }
+	}
+}
---
+++ Logging/Logging/XmlConfigured/CustomAppenderTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using log4net;
+
+namespace Logging.XmlConfigured
+{
+	[TestFixture]
+	public class CustomAppenderTests
+	{
+		[Test]
+		public void CanUseCustomAppenders()
+		{
+			var repository = LogManager.GetRepository();
+			var appenders = repository.GetAppenders();
+			var customAppenders = appenders.OfType<NoopAppender>();
+			var customAppender = customAppenders.First();
+			Assert.That(customAppender, Is.Not.Null);
+			Assert.That(customAppender.Name, Is.EqualTo("CustomAppender"));
+			Assert.That(customAppender.Token, Is.EqualTo("MyToken"));
+		}
+	}
+}
